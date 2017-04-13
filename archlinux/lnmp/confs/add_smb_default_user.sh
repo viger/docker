a0e0d6a6c02361fd2865e5c545921e903ad0fcf6
tom <viger@mchen.info>
2017-04-13 22:04:07 +0800

    add shell script for samba

---
+++ archlinux/lnmp/confs/add_smb_default_user.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+user='__USER__'
+
+hash smbpasswd &>/dev/null || {
+    echo "has not smbpasswd.exit"
+    exit
+}
+
+hash expect &>/dev/null || {
+    pacman -Sy --noconfirm expect
+}
+
+expect <<EOF
+    set send_slow {1 .1}
+	proc send {ignore arg} {
+		sleep .1
+		exp_send -s -- \$arg
+	}
+	set timeout $EXPECT_TIMEOUT
+
+    spawn smbpasswd -a $user
+    expect {
+        -exact "*password:" { send -- "123qwe!\n"; exp_continue }
+    }
+EOF
+
+ pacman -Rc --noconfirm expect
